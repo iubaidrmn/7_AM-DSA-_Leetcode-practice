34a3f08bc78b253a58fd50f6cf27933f7379b69f
Ubaid Ur Rehman <115970796+iubaidrmn@users.noreply.github.com>
2024-10-17 10:37:53 +0500

    Time: 125 ms (49.06%), Space: 12.8 MB (81.61%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            # wait while we find a non-zero element to
+            # swap with you
+            if nums[slow] != 0:
+                slow += 1
