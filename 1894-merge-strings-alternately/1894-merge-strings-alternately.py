31f9100efefcf2b1aae2109da8d9f137e1e7c353
Ubaid Ur Rehman <115970796+iubaidrmn@users.noreply.github.com>
2024-10-22 19:54:56 +0500

    Time: 17 ms (35.18%), Space: 11.8 MB (17.65%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        s = ""
+        n = min(len(word1),len(word2))
+        for i in range(n):
+            # s = s + word1[i] + word2[i]
+            s += word1[i] + word2[i]
+        if len(word1) > len(word2):
+            s += word1[n:]
+        else:
+            s += word2[n:]
+        return s 
+        
+        
